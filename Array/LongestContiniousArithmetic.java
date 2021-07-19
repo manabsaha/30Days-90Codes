f3bc2dc84b8896e455d7c6f5689b2278a2adf5eb
Manab <46393531+manabsaha@users.noreply.github.com>
2021-07-19 17:12:18 +0530

    Update LongestContiniousArithmetic.java

--- Array/LongestContiniousArithmetic.java
+++ Array/LongestContiniousArithmetic.java
@@ -1,8 +1,9 @@
-/* An arithmetic array is an array that contains at least two integers and the differences between consecutive 
+/* GOOGLE KICKSTART PROBLEM
+An arithmetic array is an array that contains at least two integers and the differences between consecutive 
 integers are equal. For example, [9, 10], [3, 3, 3], and [9, 7, 5, 3] are arithmetic arrays, while [1, 3, 3, 7], 
 [2, 1, 2], and [1, 2, 4] are not arithmetic arrays.
 
-Sarasvati has an array of N non-negative integers. The i-th integer of the array is A. She wants to choose 
+Sarasvati has an array of N non-negative integers. The i-th integer of the array is Aᵢ. She wants to choose 
 a contiguous arithmetic subarray from her array that has the maximum length. Please help her to determine the 
 length of the longest contiguous arithmetic subarray.
 */
