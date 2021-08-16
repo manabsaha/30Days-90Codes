df7c72043d12247f09d1e10c03f5e5249642ee67
Manab <46393531+manabsaha@users.noreply.github.com>
2021-08-16 12:50:43 +0530

    Create MirrorOfBT.java

---
+++ 12. Binary Tree/MirrorOfBT.java
@@ -0,0 +1,15 @@
+/* Given a Binary Tree, convert it into its mirror. */
+
+class Solution {
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node node) {
+        if(node==null){
+            return;
+        }
+        mirror(node.left);
+        mirror(node.right);
+        Node temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+    }
+}
